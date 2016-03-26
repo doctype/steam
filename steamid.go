6b95a1da6e86e420eafc7655c77cc771b2f778a7
A. Samy <f.fallen45@gmail.com>
2016-03-26 16:22:30 +0000

    steamid: add steam2/3 id parsers
    Signed-off-by: A. Samy <f.fallen45@gmail.com>

--- steamid.go
+++ steamid.go
@@ -1,6 +1,10 @@
 package steam
 
-import "strconv"
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
 
 type SteamID uint64
 
@@ -33,8 +37,94 @@ const (
 	AccountInstanceWeb
 )
 
+const (
+	ChatInstanceFlagClan     = 0x80000
+	ChatInstanceFlagLobby    = 0x40000
+	ChatInstanceFlagMMSLobby = 0x20000
+)
+
+var (
+	// See Steam Documentation for more information on how this is formatted.
+	//		STEAM_X:Y:Z
+	//	X + 1 = universe
+	//	Y = lowest bit of Account ID
+	//	Z = upper 31 bits of Account ID
+	legacyRegexp = regexp.MustCompile("STEAM_([0-5]):([0-1]):([0-9]+)")
+
+	// Modern Steam ID
+	//		[C:U:A] or [C:U:A:I]
+	//	C = account type or instance ID
+	//	U = universe
+	//	A = account ID
+	//	I = instance id if not present, the default instance id for that C value is used.
+	modernRegexp = regexp.MustCompile("\\[([a-zA-Z]):([0-5]):([0-9]+)(:[0-9]+)?\\]")
+
+	ErrInvalidSteam2ID = errors.New("invalid input specified for a Steam 2 ID")
+	ErrInvalidSteam3ID = errors.New("invalid input specified for a Steam 3 ID")
+)
+
 func (sid *SteamID) Parse(accid uint32, instance uint32, accountType uint32, universe uint8) {
 	*sid = SteamID(uint64(accid) | (uint64(instance&0xFFFFF) << 32) | (uint64(accountType) << 52) | (uint64(universe) << 56))
+}
+
+func (sid *SteamID) ParseSteam2ID(input string) error {
+	m := legacyRegexp.FindStringSubmatch(input)
+	if m == nil || len(m) < 4 {
+		return ErrInvalidSteam2ID
+	}
+
+	universe, _ := strconv.ParseUint(string(m[1]), 10, 8)
+	lobit, _ := strconv.ParseUint(string(m[2]), 10, 8)
+	hibits, _ := strconv.ParseUint(string(m[3]), 10, 32)
+
+	sid.Parse(uint32(lobit|hibits<<1), AccountInstanceDesktop, AccountTypeIndividual, uint8(universe+1))
+	return nil
+}
+
+func (sid *SteamID) ParseSteam3ID(input string) error {
+	m := modernRegexp.FindStringSubmatch(input)
+	if m == nil || len(m) < 4 {
+		return ErrInvalidSteam3ID
+	}
+
+	accountID, _ := strconv.ParseUint(string(m[3]), 10, 32)
+	universe, _ := strconv.ParseUint(string(m[2]), 10, 8)
+
+	instance := uint64(AccountInstanceDesktop)
+	if len(m) > 5 {
+		instance, _ = strconv.ParseUint(string(m[5]), 10, 32)
+	}
+
+	accountType := uint32(AccountTypeIndividual)
+	switch m[1][0] {
+	case 'c':
+		instance |= ChatInstanceFlagClan
+		accountType = AccountTypeChat
+	case 'L':
+		instance |= ChatInstanceFlagLobby
+		fallthrough
+	case 'T':
+		accountType = AccountTypeChat
+	case 'I':
+		accountType = AccountTypeInvalid
+	case 'M':
+		accountType = AccountTypeMultiSeat
+	case 'G':
+		accountType = AccountTypeGameServer
+	case 'A':
+		accountType = AccountTypeAnonymousGameServer
+	case 'P':
+		accountType = AccountTypePending
+	case 'C':
+		accountType = AccountTypeContentServer
+	case 'g':
+		accountType = AccountTypeClan
+	case 'a':
+		accountType = AccountTypeAnonymous
+	}
+
+	sid.Parse(uint32(accountID), uint32(instance), accountType, uint8(universe))
+	return nil
 }
 
 func (sid *SteamID) GetAccountID() uint32 {
