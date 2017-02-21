746321c7dcf4dff9f8234f7c373c4beb39876d48
Ahmed Samy <f.fallen45@gmail.com>
2017-02-21 18:40:53 +0000

    examples/inventory: no need for the delay, the API is now more stable

--- examples/inventory/main.go
+++ examples/inventory/main.go
@@ -43,9 +43,6 @@ func main() {
 			for _, item := range inven {
 				log.Printf("Item: %s = %d\n", item.MarketHashName, item.AssetID)
 			}
-
-			// Wait a bit so we don't get an error.
-			time.Sleep(time.Millisecond * 100)
 		}
 	}
 
